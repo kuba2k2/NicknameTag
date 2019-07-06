2c0035c5b7a75a701013058c75bc4720966110cf
kubasz <kuba@szczodrzynski.pl>
2019-07-06 13:40:49 +0200

    Initial commit

---
+++ src/main/java/pl/szczodrzynski/nicknametag/Main.java
@@ -0,0 +1,16 @@
+package pl.szczodrzynski.nicknametag;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
